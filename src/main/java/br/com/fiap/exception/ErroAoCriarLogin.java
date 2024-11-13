c0e0d69ca73b06c44876dcf40d501f4a1c6bd75c
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-11-13 19:36:28 -0300

    feat: criando classe de login

---
+++ src/main/java/br/com/fiap/exception/ErroAoCriarLogin.java
@@ -0,0 +1,8 @@
+package br.com.fiap.exception;
+
+public class ErroAoCriarLogin extends Exception {
+
+    public ErroAoCriarLogin(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/br/com/fiap/model/Login.java
@@ -0,0 +1,48 @@
+package br.com.fiap.model;
+
+import br.com.fiap.exception.ErroAoCriarLogin;
+
+public class Login {
+
+    private Long id;
+    private String email;
+    private String senha;
+
+    public Login() {}
+
+    public Login(String email, String senha) throws ErroAoCriarLogin {
+        setEmail(email);
+        setSenha(senha);
+    }
+
+
+    public void setSenha(String senha) throws ErroAoCriarLogin {
+        if (senha.length() < 6) {
+            throw new ErroAoCriarLogin("Senha deve ser maior que 6 caracteres");
+        }
+        this.senha = senha;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) throws ErroAoCriarLogin {
+        if(email.isBlank() || (email.contains("@") && email.contains(".com"))){
+            throw new ErroAoCriarLogin("Email invalido");
+        }
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+}
