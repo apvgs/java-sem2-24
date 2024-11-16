67cc9cf178612be7e04f673db9b427928ef9968a
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-11-15 23:08:11 -0300

    feat: add metodo de listar todos por id do usuario e a data

--- src/main/java/br/com/fiap/service/LeituraEnergiaService.java
+++ src/main/java/br/com/fiap/service/LeituraEnergiaService.java
@@ -1,17 +1,21 @@
 package br.com.fiap.service;
 
+import br.com.fiap.exception.ConsumoNotFound;
 import br.com.fiap.exception.CpfInvalido;
 import br.com.fiap.exception.ErroAoCriarLogin;
 import br.com.fiap.model.LeituraEnergia;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.List;
 
 public interface LeituraEnergiaService {
 
-    void inserir(LeituraEnergia leituraEnergia) throws SQLException;
+    void inserir(LeituraEnergia leituraEnergia) throws SQLException, ErroAoCriarLogin, CpfInvalido, ConsumoNotFound;
 
     LeituraEnergia buscar(Long id) throws SQLException, ErroAoCriarLogin, CpfInvalido;
 
     List<LeituraEnergia> listar(Long leituraEnergiaId) throws SQLException, ErroAoCriarLogin, CpfInvalido;
+
+    List<LeituraEnergia> listar(Long usuarioId, LocalDate data) throws SQLException, ErroAoCriarLogin, CpfInvalido;
 }
--- src/main/java/br/com/fiap/service/LeituraEnergiaServiceImpl.java
+++ src/main/java/br/com/fiap/service/LeituraEnergiaServiceImpl.java
@@ -3,25 +3,40 @@ package br.com.fiap.service;
 import br.com.fiap.config.DatabaseConnectionFactory;
 import br.com.fiap.dao.LeituraEnergiaDao;
 import br.com.fiap.dao.LeituraEnergiaDaoFactory;
+import br.com.fiap.exception.ConsumoNotFound;
 import br.com.fiap.exception.CpfInvalido;
 import br.com.fiap.exception.ErroAoCriarLogin;
+import br.com.fiap.model.ConsumoDiario;
 import br.com.fiap.model.LeituraEnergia;
 import br.com.fiap.model.Notificacao;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.List;
 
 final class LeituraEnergiaServiceImpl implements LeituraEnergiaService {
 
     private LeituraEnergiaDao leituraEnergiaDao = LeituraEnergiaDaoFactory.getLeituraEnergiaDao();
+    private ConsumoDiarioService consumoDiarioService = ConsumoDiarioServiceFactory.create();
     private NotificacaoService notificacaoService = NotificacaoServiceFactory.create();
 
     @Override
-    public void inserir(LeituraEnergia leituraEnergia) throws SQLException {
+    public void inserir(LeituraEnergia leituraEnergia) throws SQLException, ErroAoCriarLogin, CpfInvalido, ConsumoNotFound {
+        ConsumoDiario consumoDiario = consumoDiarioService.buscarPorId(leituraEnergia.getDispositivoMedicao().getEndereco().getUsuario().getId(),
+                leituraEnergia.getDataMedicao().toLocalDate());
         Connection connection = DatabaseConnectionFactory.getConnection();
         try {
             leituraEnergiaDao.inserir(connection, leituraEnergia);
+            if (consumoDiario == null){
+                consumoDiario = new ConsumoDiario();
+                consumoDiario.setEndereco(leituraEnergia.getDispositivoMedicao().getEndereco());
+                consumoDiario.setDate(leituraEnergia.getDataMedicao().toLocalDate());
+                consumoDiario.setConsumoDiario(leituraEnergia.getConsumo());
+                consumoDiarioService.cadastrar(connection, consumoDiario);
+            }
+            consumoDiario.setConsumoDiario(consumoDiario.getConsumoDiario() + leituraEnergia.getConsumo());
+            consumoDiarioService.alterar(connection, consumoDiario);
             if (leituraEnergia.getConsumo() > 700){
                 Notificacao notificacao = new Notificacao("Alerta Houve um pico de energia!!!!", leituraEnergia);
                 notificacaoService.inserir(connection, notificacao);
@@ -46,6 +61,13 @@ final class LeituraEnergiaServiceImpl implements LeituraEnergiaService {
     public List<LeituraEnergia> listar(Long enderecoId) throws SQLException, ErroAoCriarLogin, CpfInvalido {
         try(Connection connection = DatabaseConnectionFactory.getConnection()) {
             return leituraEnergiaDao.listar(connection, enderecoId);
+        }
+    }
+
+    @Override
+    public List<LeituraEnergia> listar(Long usuarioId, LocalDate data) throws SQLException, ErroAoCriarLogin, CpfInvalido {
+        try(Connection connection = DatabaseConnectionFactory.getConnection()) {
+            return leituraEnergiaDao.listar(connection, usuarioId, data);
         }
     }
 }
